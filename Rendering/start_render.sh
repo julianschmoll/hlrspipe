80875cb9dbe6ca1eddf260f46ddc31d3d07d52c7
julianschmoll <github@julianschmoll.de>
2023-01-12 23:27:36 +0100

    external status file

--- Rendering/start_render.sh
+++ Rendering/start_render.sh
@@ -2,26 +2,35 @@
 
 folder=$1
 
-for file in $folder/*.job; do
+for file in $folder/*.Job; do
     dos2unix $file
 done
 
-while [[ $(qstat | grep -c $folder) -ne 0 ]]; do
-  for file in $folder/*.job; do
-    jobname=$(basename $file .job)
-    if [[ $(qstat | grep $jobname) ]]; then
-      echo "Job $jobname already submitted."
-    else
-      echo "Submitting job $jobname."
-      qsub $file 
-        if [ $? -eq 0 ]; then
-            echo "job $file queued"
-        else
-            echo " queue limit reached finish for now."
-        fi 
-    fi
-  done
-  sleep 180 
-done
+status_file="$folder/status.txt"
+touch $status_file
 
-echo "All jobs in $folder have been submitted"
+while true; do
+    all_submitted=true
+    for file in $folder/*.Job; do
+        jobname=$(basename $file .Job)
+        if grep -q $jobname $status_file; then
+            echo "Job $jobname already submitted."
+        else
+            all_submitted=false
+            echo "Submitting job $jobname."
+            qsub $file
+            if [ $? -eq 0 ]; then
+                echo "job $file queued"
+                echo $jobname >> $status_file
+            else
+                echo " queue limit reached finish for now."
+            fi 
+        fi
+    done
+    if [ "$all_submitted" = true ]; then
+        echo "All jobs in $folder have been submitted"
+        break
+    else
+        sleep 180 
+    fi
+done

6664f5b7858878ca95024ce5f1f597f229d3884a
julianschmoll <github@julianschmoll.de>
2023-01-12 14:33:26 +0100

    looped until all jobs are submitted

--- Rendering/start_render.sh
+++ Rendering/start_render.sh
@@ -1,41 +1,23 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]; then
-  echo "ERROR $0: need .ass Directory"
-  exit 1
-fi
+folder=$1
 
-SEQUENZ_NAME=$1
-
-ARNOLD_WORK_SPACE=/lustre/hpe/ws10/ws10.1/ws/zmcbeber-workspace1/zmcbeber-workspace1/sequenzen
-ARNOLD_ROOT=/zhome/academic/HLRS/zmc/zmcbeber/Arnold_SDK-7.1.3.1_Linux
-
-# Location of ass files that should be rendered
-SEQ_DIR_NAME=${ARNOLD_WORK_SPACE}/${SEQUENZ_NAME}
-
-# Location of .job files
-JOB_DIR=${ARNOLD_ROOT}/Job_dir/${SEQUENZ_NAME}
-
-if [ ! -d ${SEQ_DIR_NAME} ]; then
-   echo " Error directory ${SEQ_DIR_NAME} is not available"
-   exit 1
-fi
-
-cd ${JOB_DIR}
-if [ $? -ne 0 ]; then
-    echo "ERROR: Can't change into Job directory ${JOB_DIR}"
-    exit
-fi
-
-for x in `ls -1 ${JOB_DIR}`; do    
-    qsub $x
-    if [ $? -eq 0 ]; then
-            echo "job ${x} queued"
+while [[ $(qstat | grep -c $folder) -ne 0 ]]; do
+  for file in $folder/*.job; do
+    jobname=$(basename $file .job)
+    if [[ $(qstat | grep $jobname) ]]; then
+      echo "Job $jobname already submitted."
+    else
+      echo "Submitting job $jobname."
+      qsub $file 
+        if [ $? -eq 0 ]; then
+            echo "job $file queued"
         else
             echo " queue limit reached finish for now."
-            exit 0
         fi 
     fi
+  done
+  sleep 180 
 done
 
-exit 0
+echo "All jobs in $folder have been submitted"

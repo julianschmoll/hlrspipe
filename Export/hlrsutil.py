a06a57da2bab6ebd0292d66374ff21f5e343a9f8
julianschmoll <github@julianschmoll.de>
2023-01-18 15:26:16 +0100

    Revert "Merge pull request #1 from julianschmoll/feat/threaded-copy"
    This reverts commit 6755214186dd912a8694ba5665fd40ec25f332d8, reversing
    changes made to 2c60d619b25f162fa5a3d63c1da8af372e0d26fc.

--- Export/hlrsutil.py
+++ Export/hlrsutil.py
@@ -1,20 +1,10 @@
 import pymel.core as pc
 import json
 import os
-import shutil
-import time
+import subprocess
 
-from concurrent import futures
 from pathlib import Path
-
-
-def copy_worker(src_and_target: tuple):
-    source, target = src_and_target
-    print(f"{source} - Copying")
-    try:
-        shutil.copy2(source, target)
-    except (FileNotFoundError, PermissionError) as e:
-        print(f"{source} - ERROR: {e}")
+from shutil import copy
 
 
 def collect_files(dest: str, image_suffix_list : list = None) -> list:
@@ -54,18 +44,16 @@ def collect_files(dest: str, image_suffix_list : list = None) -> list:
         filepath = Path(standin.dso.get())
         filepaths.append(filepath)
         folders.add(filepath.parent)
-    
-    print("starting copy threads ...")
-    with futures.ThreadPoolExecutor() as exec:
-        jobs = [
-            exec.submit(copy_worker, (Path(src), dest / Path(src).name))
-            for src in set(filepaths)
-        ]
-        for job in futures.as_completed(jobs):
-            # Nothing to do here really, we just don't want to use
-            # futures.wait() since that could freeze Maya.
-            time.sleep(1)
-            pass
+        
+    for filepath in set(filepaths):
+        src = Path(filepath)
+        try:
+            copy(src, dest / src.name)
+            print(f"Copying {src}")
+        except FileNotFoundError:
+            print(f"File {filepath} not found") 
+        except PermissionError:
+            print(f"No permission for {filepath}")
 
     return folders
 
